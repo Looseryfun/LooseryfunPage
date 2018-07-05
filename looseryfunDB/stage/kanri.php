dbc1d7cee3834e24486827732c5b8100aa044eb0
GeraHa <geraha9@gmail.com>
2018-07-05 10:00:44 +0900

    APC管理画面追加

--- looseryfunDB/stage/kanri.php
+++ looseryfunDB/stage/kanri.php
@@ -6,6 +6,7 @@
 	<div class="title-1"><h3 id="content_2">管理ページ</h3></div>
 	<ul class="list-1" id="356283_block_2">
 		<li><a href="phpinfo.php">php情報</a></li>
+		<li><a href="apc.php">キャッシュメモリ管理</a></li>
 		<li><a href="http://looseryfun.s1006.xrea.com/log/phpmyadmin/index.php">データベース管理</a></li>
 		<li><a href="https://cp.xrea.com/">サーバー管理</a></li>
 		<li><a href="manageuser.php">ユーザー管理</a></li>
