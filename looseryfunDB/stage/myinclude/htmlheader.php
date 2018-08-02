37d47979ba92f36b542a6e6ecc385b0cbdf3478e
GeraHa <geraha9@gmail.com>
2018-08-02 15:37:30 +0900

    タイトルなど変更可能に

--- looseryfunDB/stage/myinclude/htmlheader.php
+++ looseryfunDB/stage/myinclude/htmlheader.php
@@ -4,13 +4,14 @@
 	if(strpos($_SERVER["REQUEST_URI"],'myinclude')!==false){
 		exit();
 	}
+	if(!isset($html_title))$html_title = 'Loosery funデータベースサービス';
 ?>
 <html lang="ja">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="Content-Style-Type" content="text/css" />
 	<meta http-equiv="Content-Script-Type" content="text/javascript" />
-	<title>Loosery funnデータベースサービス</title>
+	<title><?php echo $html_title; ?></title>
 	<link href="https://image02.seesaawiki.jp/l/o/looseryfun-game-info/4ec40051ee89d618b1b5.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
 	<link href="https://image02.seesaawiki.jp/l/o/looseryfun-game-info/4ec40051ee89d618b1b5.png" rel="apple-touch-icon">
 	<link rel="stylesheet" type="text/css" href="https://static.seesaawiki.jp/css/usr/common.css" />
@@ -20,7 +21,8 @@
 	<script type="text/javascript" src="https://static.seesaawiki.jp/js/jquery/jquery-1.11.0.min.js"></script>
 	<script type="text/javascript" src="https://static.seesaawiki.jp/js/usr/second/wikier.js"></script>
 	<script type="text/javascript" src="looseryfundb.js"></script>
-	<meta name="description" content="Loosery funデータベースサービス" />
+	<meta name="description" content="<?php echo $html_title; ?>" />
+	<?php if(isset($html_head))echo $html_head; ?>
 <style type="text/css">  
 <!-- 
 	div#container
